2413d3420198634699d7f322c604df5d4b2814ad
ShritamMohapatra <shritamm8@gmail.com>
2022-02-23 17:41:56 +0530

    1st solution

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+router.get('/sol1', function (req, res) {
+    let arr = [1,2,3,4,5,7,8,9]
+    let arrSum = 0;
+    let sum = 0;
+    
+    for(let i =0;i<arr.length;i++){
+        sum = arr[i]*(arr[i]+1)/2
+    }
+
+    for(let i = 0;i<arr.length;i++){
+        arrSum += arr[i]
+    }
+    let missingNumber = sum - arrSum;
+    res.send({data:missingNumber});
 });
 
 module.exports = router;
